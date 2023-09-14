6188303d9cb404e99f88dc0534530bf484380391
jota5800 <jota5800@colorado.edu>
2023-09-14 17:49:57 -0600

    Version - jonah : 1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ std::vector<bool> LessMask(std::vector<double> nums, double less_than);
 // returns a vector with true for numbers greater than the second parameters and false for those less than or equal to
 std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
 
-int main(){
+int main()
+{
     //call each of the above functions
     return 1;
 }
