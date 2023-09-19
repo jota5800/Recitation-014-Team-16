a038488eba7a4c92cc6f63635bd22df74fc3d551
Oscar Guerrero <osgu7204@colorado.edu>
2023-09-19 12:35:47 -0600

    Version - Oscar : Return 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 
@@ -10,19 +9,5 @@ std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
 
 int main(){
     //call each of the above functions
-    return 2;
-=======
-#include <iostream>
-#include <vector>
-
-// returns a vector with true for numbers less than the second parameters and false for those greater than or equal to
-std::vector<bool> LessMask(std::vector<double> nums, double less_than);
-
-// returns a vector with true for numbers greater than the second parameters and false for those less than or equal to
-std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
-
-int main(){
-    //call each of the above functions
-    return 0;
->>>>>>> fa42d4a1ae1961be0eeb3d279c89f1df8e4b3f9e
+    return 1;
 }
